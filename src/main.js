e4ab69173f82b16aba982edb50789df9fac0d21d
smidkristian <smid.kristian@gmail.com>
2021-03-17 19:51:27 +0100

    initial set-up

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
+
+import './assets/tailwind.css'
 
 Vue.config.productionTip = false
 
